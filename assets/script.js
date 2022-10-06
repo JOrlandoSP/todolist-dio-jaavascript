ff3199b11216d4c6242b29cb3c19dce986cc78bd
JOrlandoSP <j.orlando.ict@gmail.com>
2022-10-06 01:26:30 -0300

    modified:   assets/script.js 	modified:   index.html

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,26 @@
 let INPUT = document.getElementById("input")
 let BTNEL = document.getElementById("btn")
 let LISTEL = document.getElementById("list")
+let counter =0;
+
+
+//Detalhe para o string literals utilizado para gerar uma função anonima para cara elemento
+//No momento de definir a função precisei utilizar aspas simples para que o evento onclick
+//montasse a função anonima da forma correta
+//
+//TO-DO adicionar um botão que adiciona um marcador para itens completados 
 
 BTNEL.addEventListener("click", ()=> {
-        LISTEL.innerHTML += `<li> ${INPUT.textContent} </li>`;
-        INPUT.innerText = null;
+	counter +=1
+    LISTEL.innerHTML += 
+		`
+		<li id="${counter}" class="itemLi">${INPUT.value} <button id="${counter}" 
+		onclick='
+		(function(){ document.getElementById("${counter}").remove(); })();
+		'>Remove</button> </li>
+		`;
+    INPUT.innerText = null;
+		
+
 })
+
