9e42a8cd58294b01effb7f2cae3a864da19c2114
JOrlandoSP <j.orlando.ict@gmail.com>
2022-10-06 12:57:39 -0300

    modified:   assets/script.js

--- assets/script.js
+++ assets/script.js
@@ -10,17 +10,41 @@ let counter =0;
 //
 //TO-DO adicionar um botão que adiciona um marcador para itens completados 
 
+INPUT.focus();
+
 BTNEL.addEventListener("click", ()=> {
 	counter +=1
     LISTEL.innerHTML += 
 		`
-		<li id="${counter}" class="itemLi">${INPUT.value} <button id="${counter}" 
+		<li id="${counter}" class="itemLi">${INPUT.value}
+
+		<button class="done"
+		onclick='
+		(function(){document.getElementById("${counter}").style.backgroundColor = "red"; })();
+		'>Done</button>
+		
+		<button class="undone"
+		onclick='
+		(function(){document.getElementById("${counter}").style.backgroundColor = "white"; })();
+		'>Undone</button>
+
+		<button class="delete "id="${counter}" 
 		onclick='
 		(function(){ document.getElementById("${counter}").remove(); })();
 		'>Remove</button> </li>
 		`;
-    INPUT.innerText = null;
+    INPUT.value = "";
+	INPUT.focus();
 		
 
-})
+});
 
+//Adicionado a funcionalidade de adicionar o evento pressionando a telca "Enter"
+//
+
+INPUT.addEventListener("keypress", (event)=>{
+	if (event.key === "Enter") {
+		event.preventDefault();
+		BTNEL.click();
+	}
+});
