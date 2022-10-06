02ad34bcfefad5ff337c8208e9b2e1eb5122191f
JOrlandoSP <j.orlando.ict@gmail.com>
2022-10-06 13:20:28 -0300

    modified:   assets/script.js

--- assets/script.js
+++ assets/script.js
@@ -6,38 +6,39 @@ let counter =0;
 
 //Detalhe para o string literals utilizado para gerar uma função anonima para cara elemento
 //No momento de definir a função precisei utilizar aspas simples para que o evento onclick
-//montasse a função anonima da forma correta
-//
-//TO-DO adicionar um botão que adiciona um marcador para itens completados 
+//monte a função anonima da forma correta detro do "li" criado
 
 INPUT.focus();
 
 BTNEL.addEventListener("click", ()=> {
-	counter +=1
-    LISTEL.innerHTML += 
+	if (INPUT.value === ""){
+	alert("Insert a task to add into the list")
+	} else {
+		counter +=1
+		LISTEL.innerHTML += 
 		`
-		<li id="${counter}" class="itemLi">${INPUT.value}
-
-		<button class="done"
-		onclick='
-		(function(){document.getElementById("${counter}").style.backgroundColor = "red"; })();
-		'>Done</button>
+			<li id="${counter}" class="itemLi">
+			<button class="done"
+			onclick='
+			(function(){document.getElementById("${counter}").style.backgroundColor = "red"; })();
+			'>Done</button>
 		
-		<button class="undone"
-		onclick='
-		(function(){document.getElementById("${counter}").style.backgroundColor = "white"; })();
-		'>Undone</button>
+			<button class="undone"
+			onclick='
+			(function(){document.getElementById("${counter}").style.backgroundColor = "white"; })();
+			'>Undone</button>
 
-		<button class="delete "id="${counter}" 
-		onclick='
-		(function(){ document.getElementById("${counter}").remove(); })();
-		'>Remove</button> </li>
-		`;
-    INPUT.value = "";
-	INPUT.focus();
-		
+			<button class="delete "id="${counter}" 
+			onclick='
+			(function(){ document.getElementById("${counter}").remove(); })();
+			'>Remove</button> 
 
-});
+			${INPUT.value} </li>
+			`;
+		INPUT.value = "";
+		INPUT.focus();
+			}
+	});
 
 //Adicionado a funcionalidade de adicionar o evento pressionando a telca "Enter"
 //
